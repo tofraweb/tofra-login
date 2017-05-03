550dd806abae46af9a6bd509379cfe1d3c81ce6c
tofra <tofraweb@gmail.com>
2017-05-03 11:37:11 +0300

    css file and img additions

---
+++ application/views/inc/footer.php
@@ -0,0 +1,19 @@
+</div> <!-- End of content -->
+
+<div class="footer">
+
+  <div class="wrapper">
+
+    <ul>
+      <li><a href="http://twitter.com/treehouse">Twitter</a></li>
+      <li><a href="https://www.facebook.com/TeamTreehouse">Facebook</a></li>
+    </ul>
+
+    <p>&copy;2015 Personal Media Library</p>
+
+  </div>
+
+</div>
+
+</body>
+</html>
